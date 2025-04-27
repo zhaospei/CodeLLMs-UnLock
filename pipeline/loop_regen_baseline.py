683f4bdeb37cc98f2e21b01d497c5157cc05cab6
trang-n <>
2025-04-28 06:18:27 +0900

    ahsjdah

--- pipeline/loop_regen_baseline.py
+++ pipeline/loop_regen_baseline.py
@@ -147,40 +147,43 @@ def main(args):
     continue_re_gen_from = args.file_path
     df = pd.read_parquet(continue_re_gen_from)
 
+    df = df.sample(n=10)
     for loop_id in range(args.num_loops):
         print("Loop ID:", loop_id)
         predictions = []
         regen_codes = []
         embeddings = []
         
-        for _, row in df.iterrows():
-            if loop_id == 0:
-                embedding = torch.tensor(row['last_token_code_embedding']).float().to("cuda")
-            else:
-                if row[f'predictions_{loop_id}'] == 0.0:
-                    embedding = torch.tensor(row[f'last_token_code_embedding_{loop_id}']).float().to("cuda")
+        with tqdm(total=len(df)) as pbar:
+            for _, row in df.iterrows():
+                if loop_id == 0:
+                    embedding = torch.tensor(row['last_token_code_embedding']).float().to("cuda")
                 else:
-                    print("No need to regenerate for task_id:", row['task_id'])
-                    predictions.append(1.0)
-                    regen_codes.append("")
-                    embeddings.append(None)
-                    continue
-            with torch.no_grad():
-                output = clf_model(embedding)
-                pred = (output > 0.5).float().item()
-                if pred == 0.0:
-                    output, last_code_token_last_layer_embedding = eval_prompt(row['task_id'], row['extracted_code'])
-                    print("Regenerated code for task_id:", row['task_id'])
-                    print("Generated code:", row['extracted_code'])
-                    print("Regen code:", output)
-                    predictions.append(pred)
-                    regen_codes.append(output)
-                    embeddings.append(last_code_token_last_layer_embedding)
-                else:
-                    print("No need to regenerate for task_id:", row['task_id'])
-                    predictions.append(pred)
-                    regen_codes.append("")
-                    embeddings.append(None)
+                    if row[f'predictions_{loop_id}'] == 0.0:
+                        embedding = torch.tensor(row[f'last_token_code_embedding_{loop_id}']).float().to("cuda")
+                    else:
+                        print("No need to regenerate for task_id:", row['task_id'])
+                        predictions.append(1.0)
+                        regen_codes.append("")
+                        embeddings.append(None)
+                        continue
+                with torch.no_grad():
+                    output = clf_model(embedding)
+                    pred = (output > 0.5).float().item()
+                    if pred == 0.0:
+                        output, last_code_token_last_layer_embedding = eval_prompt(row['task_id'], row['extracted_code'])
+                        print("Regenerated code for task_id:", row['task_id'])
+                        print("Generated code:", row['extracted_code'])
+                        print("Regen code:", output)
+                        predictions.append(pred)
+                        regen_codes.append(output)
+                        embeddings.append(last_code_token_last_layer_embedding)
+                    else:
+                        print("No need to regenerate for task_id:", row['task_id'])
+                        predictions.append(pred)
+                        regen_codes.append("")
+                        embeddings.append(None)
+                pbar.update(1)
         df[f"predictions_{loop_id + 1}"] = predictions
         df[f"regen_codes_{loop_id + 1}"] = regen_codes
         df[f"last_token_code_embedding_{loop_id + 1}"] = embeddings
